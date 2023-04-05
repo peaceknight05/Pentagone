9cd608ec5b832c73c367d83265e95deb448ddf6b
Arash <47273556+arashnrim@users.noreply.github.com>
2023-04-05 03:19:04 +0000

    Add Deck model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Deck(models.Model):
+    words = models.JSONField()
+    cache = models.JSONField()
+    threshold = models.DecimalField(max_digits=2, decimal_places=1)
